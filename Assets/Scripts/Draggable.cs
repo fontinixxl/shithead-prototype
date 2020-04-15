40d32e3c902a5a756313d166ae48ea0312bd0514
Gerard Cuello <fontinixxl@users.noreply.github.com>
2020-04-15 22:52:05 +0200

    Once the card is dropped into the DragZone, the card is no longer draggable

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [HideInInspector] public Transform parentToReturnTo;
+
+    public float minCardRotation = -15.0f;
+    public float maxCardRotation = 15.0f;
+
+    private HorizontalLayoutGroup layout;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -16,6 +22,9 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         // Once we begin dragging, change the current parent to the Canvas (parent->parent)
         // This will make the panel re-arrange the current items and deatach the object from it.
         transform.SetParent(transform.parent.parent);
+
+        // Apply a random rotation on the Z axi as soon as we start draggin the card.
+        transform.Rotate(0.0f, 0.0f, Random.Range(minCardRotation, maxCardRotation));
 
         // In order to dettect the "Drop Zone" we need to have the Raycast on (off by default)
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -31,7 +40,17 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         // Set the parent back to the original in case the object hasn't been dropped properly.
         transform.SetParent(parentToReturnTo);
 
+        // If at this point the parent of the Draggable is the Hand
+        // meaning we haven't dropped the card to the DropZone, we set the rotation back to 0.
+        // TODO: Refactor-> look for a better way to dettect that the card is back to the Hand
+        if (transform.parent.name == "Hand")
+        {
+            transform.rotation = Quaternion.identity;
+        }
+
         // Set back to the original value; to block Raycasts
+        // TODO: Change that
+        if (parentToReturnTo.name != "DropZone")
         GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -6,19 +6,14 @@ using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour, IDropHandler
 {
-    public int spaceingFactor = 9;
-
-    private HorizontalLayoutGroup layout;
-
     public void Start()
     {
-        layout =GetComponent<HorizontalLayoutGroup>();
     }
     public void OnDrop(PointerEventData eventData)
     {
         //Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
 
-        // Get the draggable object from the eventData (the object the player is draggin to the dropZone)
+        // Get the draggable object from the eventData (the object the player is dragging to the dropZone)
         Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
         if (draggable != null)
         {
@@ -29,13 +24,6 @@ public class DropZone : MonoBehaviour, IDropHandler
             // Set the draggable object position the the dragZone so all the draggable object
             // will stack on it; on on the top of each other.
             draggable.transform.position = transform.position;
-        }
-        
-        // If the dropZone we has a layout
-        if (layout != null)
-        {
-
-            layout.spacing -= spaceingFactor;
         }
     }
 }
