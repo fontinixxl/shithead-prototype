fab26a98f6ab2dc3fce86885cd9653b4c0528d6f
Gerard Cuello <fontinixxl@users.noreply.github.com>
2020-04-15 17:57:36 +0200

    Enable 'basic' drag and drop from HandZone to the Drop Zone and viceversa

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [HideInInspector] public Transform parentToReturnTo;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnBeginDrag");
+
+        // Save the original parent
+        parentToReturnTo = transform.parent;
+        // Once we begin dragging, change the current parent to the Canvas (parent->parent)
+        // This will make the panel re-arrange the current items and deatach the object from it.
+        transform.SetParent(transform.parent.parent);
+
+        // In order to dettect the "Drop Zone" we need to have the Raycast on (off by default)
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Set the parent back to the original in case the object hasn't been dropped properly.
+        transform.SetParent(parentToReturnTo);
+
+        // Set back to the original value; to block Raycasts
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        // Get the draggable object from the eventData (the object the player is draggin to the dropZone)
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggable != null)
+        {
+            // Set the parentToReturnto the "DragZone" (so it doesn't get back to the HandZone)
+            // This is because the Draggable's OnEndDrag() method, by default, resets the parent 
+            // to the original (the one it had when OnBeginDrag() was firts called)
+            draggable.parentToReturnTo = transform;
+            // Set the draggable object position the the dragZone so all the draggable object
+            // will stack on it; on on the top of each other.
+            draggable.transform.position = transform.position;
+        }
+    }
+}
